c7061afcc594643fa84c0f95b1b2681ca30ce74b
Jacob Potter <jacob@durbatuluk.us>
2015-08-01 14:20:42 -0700

    Use hsv for rainbow

--- sample_patterns.c
+++ sample_patterns.c
@@ -15,12 +15,42 @@ PATTERN(blink) {
     return out;
 }
 
+rgb_t hsv(double h, double s, double v) {
+    rgb_t out = { 0, 0, 0 };
+    double r, g, b, f, p, q, t;
+    int i;
+
+    /* If case we got a wacky value of h, wrap it. */
+    if (h < 0 || h > 1) {
+        h = fmod(h, 1.0);
+        if (h < 0) {
+            h += 1;
+        }
+    }
+
+    i = h * 6;
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+    }
+
+    out.r = r * 255;
+    out.g = g * 255;
+    out.b = b * 255;
+    return out;
+}
+
 #define BRIGHTNESS 255
 PATTERN(simple_rainbow) {
     float theta = -(pixel_number * .2) + beat_counter;
-    rgb_t out = {0, 0, 0};
-    out.r = ((sin(theta) + 1) / 2) * BRIGHTNESS;
-    out.g = ((sin(theta + M_PI*2.0/3.0) + 1) / 2) * BRIGHTNESS;
-    out.b = ((sin(theta + M_PI*4.0/3.0) + 1) / 2) * BRIGHTNESS;
-    return out;
+    return hsv(theta / (2*M_PI), 1, 1);
 }
